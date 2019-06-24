a4265cd3488e59402f30093ea20273e1b6b8426b
yegonke <49680358+yegonke@users.noreply.github.com>
2019-06-24 17:31:51 +0300

    Models licitguides.xyz

--- base/models.py
+++ base/models.py
@@ -9,15 +9,4 @@ class Blog(models.Model):
     source_url = models.CharField(max_length=300)
     emb_link = models.CharField(max_length=300)
     content = models.TextField(blank=True, null=True)
-    img_url = models.ImageField(upload_to="web_img", blank=True)
-    date_added = models.DateTimeField()
-    merits = models.CharField(max_length=300, blank=True)
-    demerits = models.CharField(max_length=300, blank=True)
-    src_model = models.TextField(blank=True, null=True)
-    src_view = models.TextField(blank=True, null=True)
-    src_uri = models.TextField(blank=True, null=True)
-    forum_url = models.TextField(blank=True, null=True)
-    class Meta:
-        managed = False
-        db_table = 'blog'
-        verbose_name_plural = "Blog"
+    
