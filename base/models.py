24543b1cbcd8fd904d7ef400ef955f525266e9aa
yegonke <49680358+yegonke@users.noreply.github.com>
2019-06-24 14:22:58 +0300

    model licitguides.xyz

--- base/models.py
+++ base/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    category = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    source_url = models.CharField(max_length=300)
+    emb_link = models.CharField(max_length=300)
+    content = models.TextField(blank=True, null=True)
+    img_url = models.ImageField(upload_to="web_img", blank=True)
+    date_added = models.DateTimeField()
+    merits = models.CharField(max_length=300, blank=True)
+    demerits = models.CharField(max_length=300, blank=True)
+    src_model = models.TextField(blank=True, null=True)
+    src_view = models.TextField(blank=True, null=True)
+    src_uri = models.TextField(blank=True, null=True)
+    forum_url = models.TextField(blank=True, null=True)
+    class Meta:
+        managed = False
+        db_table = 'website_blog'
+        verbose_name_plural = "Blog"
