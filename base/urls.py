350c76c5a71c653703b26229df47d82944f409b2
yegonke <49680358+yegonke@users.noreply.github.com>
2019-06-30 10:40:16 +0300

    SEO_NULL licitguides.xyz

--- base/urls.py
+++ base/urls.py
@@ -1,15 +1,11 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+
 from . import views
-from rollyourown.seo.admin import register_seo_admin
-from django.contrib import admin
-from .seo import MyMetadata
 
 urlpatterns = [
     path('', views.index, name='index'),
 ]
-
-register_seo_admin(admin.site, MyMetadata)
 
 
